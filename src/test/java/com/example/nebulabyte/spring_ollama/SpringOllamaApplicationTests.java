504e0ca5391d2480a106fb6728a8e964b020dead
kush garg <kushgarg5113@gmail.com>
2026-03-01 13:38:31 +0530

    first commit

---
+++ src/test/java/com/example/nebulabyte/spring_ollama/SpringOllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nebulabyte.spring_ollama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOllamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
